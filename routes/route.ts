8024ffab7d0fc502be47d1a22c0fc27bdeb3cdd2
ThugRaven <kamilo1715@wp.pl>
2022-12-07 22:40:00 +0100

    feat: implement path finding to route get endpoint

--- routes/route.ts
+++ routes/route.ts
@@ -1,13 +1,36 @@
 import express from 'express';
+import features from '../features.json';
+import { createGraph, findPath, Node } from '../utils/path-finding';
+
 const router = express.Router();
+const graph = createGraph();
 
 router.get('/:query', (req, res) => {
   const query = req.params.query;
-  const nodes = query.split(';');
+  const nodeNames = query.split(';');
+  const nodes: Node[] = [];
 
-  console.log(nodes);
-  res.status(200).send({
-    route: nodes,
+  nodeNames.forEach((name) => {
+    const node = features.nodes.find(
+      (node) => node.name.trim().toLowerCase() == name.trim().toLowerCase(),
+    );
+    if (node) {
+      nodes.push(node);
+    }
+  });
+
+  if (nodes.length > 1) {
+    const route = findPath(graph, nodes);
+    return res.status(200).send({
+      route: route,
+    });
+    // console.log(route);
+  }
+
+  // console.log(nodes);
+  res.status(404).send({
+    status: 404,
+    message: 'Route not found',
   });
 });
 
