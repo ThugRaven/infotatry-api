5778393eb7e2a175925d58c55b97249db80bb1b2
ThugRaven <kamilo1715@wp.pl>
2022-12-09 21:31:04 +0100

    perf: convert path finder to class with maps

--- routes/route.ts
+++ routes/route.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import features from '../features.json';
-import { createGraph, getRoute, Node } from '../utils/path-finding';
+import PathFinder, { Node } from '../utils/PathFinder';
 
 const router = express.Router();
-const graph = createGraph();
+// const graph = createGraph();
+const pathFinder = new PathFinder();
 
 router.get('/:query', (req, res) => {
   const query = req.params.query;
@@ -20,7 +21,8 @@ router.get('/:query', (req, res) => {
   });
 
   if (nodes.length > 1) {
-    const route = getRoute(graph, nodes);
+    // const route = getRoute(graph, nodes);
+    const route = pathFinder.getRoute(nodes);
     if (route) {
       return res.status(200).send({
         route: route,
