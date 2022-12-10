8d7f15331f5b2ff963f3195c3382ea1625755bc2
ThugRaven <kamilo1715@wp.pl>
2022-12-10 19:17:56 +0100

    refactor: use `cors` package for cors headers

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,11 @@
+import cors from 'cors';
 import express from 'express';
 import route from '../routes/route';
 
 const app = express();
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type',
-  );
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  next();
-});
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.listen(8080, () => {
   console.log('Server started at http://localhost:8080');
