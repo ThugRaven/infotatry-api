79d046645bdcb949ba121ec8da76f4c7587c1753
ThugRaven <kamilo1715@wp.pl>
2022-12-14 21:08:35 +0100

    feat: add mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import cors from 'cors';
+import * as dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import route from '../routes/route';
 
+dotenv.config();
+
 const app = express();
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DB_URL!);
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Server connected to the database'));
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
