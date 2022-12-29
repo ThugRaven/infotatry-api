4c8991c7ca6e8bf0097691cc6d08820ccbb43123
ThugRaven <kamilo1715@wp.pl>
2022-12-30 00:50:45 +0100

    refactor: use timing safe password comparison

--- utils/password-utils.ts
+++ utils/password-utils.ts
@@ -2,11 +2,21 @@ import crypto from 'node:crypto';
 import util from 'node:util';
 
 const pbkdf2 = util.promisify(crypto.pbkdf2);
-export async function hashPassword(password: string, salt?: string) {
+export async function getPasswordBuffer(password: string, salt?: string) {
   const passwordSalt = salt ?? crypto.randomBytes(128).toString('base64');
-  const hash = await pbkdf2(password, passwordSalt, 120000, 64, 'sha512');
-  const hashedPassword = hash.toString('hex');
-  return `${passwordSalt}:${hashedPassword}`;
+  const buffer = await pbkdf2(password, passwordSalt, 120000, 64, 'sha512');
+  return {
+    buffer,
+    salt: passwordSalt,
+  };
+}
+
+export async function hashPassword(password: string, salt?: string) {
+  const { buffer, salt: passwordSalt } = await getPasswordBuffer(
+    password,
+    salt,
+  );
+  return `${passwordSalt}:${buffer.toString('hex')}`;
 }
 
 export async function comparePasswords(
@@ -14,6 +24,16 @@ export async function comparePasswords(
   plainPassword: string,
 ) {
   const salt = hashedPassword.split(':')[0];
-  const hashedPlainPassword = await hashPassword(plainPassword, salt);
-  return hashedPassword === hashedPlainPassword;
+  const hashedPasswordBuffer = Buffer.from(hashedPassword.split(':')[1], 'hex');
+  const { buffer: plainPasswordBuffer } = await getPasswordBuffer(
+    plainPassword,
+    salt,
+  );
+
+  try {
+    return crypto.timingSafeEqual(hashedPasswordBuffer, plainPasswordBuffer);
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
