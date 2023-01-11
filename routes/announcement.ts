38c12ecd17f3e79b5d8a0204e0ef2a9eaec2066e
ThugRaven <kamilo1715@wp.pl>
2023-01-11 22:07:44 +0100

    feat: add post endpoint for announcement

--- routes/announcement.ts
+++ routes/announcement.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { Announcement } from '../models/announcement';
+import { isAuthenticated } from './auth';
 
 const router = express.Router();
 
@@ -16,6 +17,51 @@ router.get('/history', async (req, res) => {
   const announcements = await Announcement.find();
 
   return res.status(200).send(announcements);
+});
+
+router.post('/', isAuthenticated, async (req, res) => {
+  const {
+    type,
+    title,
+    featuresType,
+    featuresIds: featuresIdsString,
+    reason,
+    since,
+    until,
+    link,
+    description,
+  } = req.body;
+
+  let featuresIds: number[] = [];
+  featuresIdsString.split(',').forEach((id: string) => {
+    featuresIds.push(parseInt(id));
+  });
+  console.log(req.body);
+  console.log(featuresIds);
+
+  const announcement = new Announcement({
+    type,
+    title,
+    featuresType,
+    featuresIds,
+    reason,
+    since,
+    until,
+    link,
+    description,
+  });
+
+  try {
+    const newAnnouncement = await announcement.save();
+    return res.status(201).send(newAnnouncement);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).send({
+        status: 400,
+        message: error.message,
+      });
+    }
+  }
 });
 
 export default router;
