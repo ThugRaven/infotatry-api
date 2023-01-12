7e52a2d920b73ed17d9fb90542d27872cf50deba
ThugRaven <kamilo1715@wp.pl>
2023-01-12 21:49:20 +0100

    feat: add closed trails avoidance for path finding

--- utils/PathFinder.ts
+++ utils/PathFinder.ts
@@ -88,6 +88,7 @@ export default class PathFinder {
   graph = new Graph();
 
   constructor() {
+    console.log('Initialize PathFinder');
     this.createGraph();
   }
 
@@ -149,8 +150,6 @@ export default class PathFinder {
   }
 
   getRoute(routeNodes: Node[]): Route | null {
-    console.log(mapFeatures.trails.get(17));
-
     if (routeNodes.length > 0) {
       const route: Route = {
         name: {
@@ -214,6 +213,108 @@ export default class PathFinder {
     } else {
       return null;
     }
+  }
+
+  getRoutes(
+    routeNodes: Node[], // :Route | null
+  ) {
+    const MAX_PATH_FINDING_RETRIES = 3;
+    const routes: Route[] = [];
+    let passedClosedTrail = false;
+
+    if (routeNodes.length > 0) {
+      for (let i = 0; i < MAX_PATH_FINDING_RETRIES; i++) {
+        passedClosedTrail = false;
+
+        const route: Route = {
+          name: {
+            start: routeNodes[0].name,
+            end: routeNodes[routeNodes.length - 1].name,
+          },
+          trails: [],
+          distance: 0,
+          time: 0,
+          ascent: 0,
+          descent: 0,
+          weatherSite: null,
+        };
+
+        let highestNode: Node | null = null;
+
+        console.log(
+          `i: ${i}, passedClosedTrail: ${passedClosedTrail}, avoidClosedTrails: ${
+            i > 0 ? true : false
+          }`,
+        );
+        for (let j = 0; j < routeNodes.length - 1; j++) {
+          const node = routeNodes[j];
+          const nextNode = routeNodes[j + 1];
+          let segment: PathSegment | null = null;
+          const foundPath = this.findPath2(
+            node,
+            nextNode,
+            false,
+            i > 0 ? true : false,
+          );
+
+          if (foundPath) {
+            passedClosedTrail = passedClosedTrail
+              ? passedClosedTrail
+              : foundPath.passedClosedTrail;
+            console.log('passedClosedTrail', passedClosedTrail);
+          } else {
+            console.log('Route not found');
+          }
+          if (foundPath && foundPath.path) {
+            segment = foundPath.path as PathSegment;
+            route.trails.push(...segment.trails);
+            route.distance += segment.distance;
+            route.time += segment.time;
+            route.ascent += segment.ascent;
+            route.descent += segment.descent;
+
+            if (segment.highestNode) {
+              if (
+                !highestNode ||
+                segment.highestNode.elevation > highestNode?.elevation
+              ) {
+                highestNode = segment.highestNode;
+              }
+            }
+          } else return null;
+        }
+
+        const lastNode = routeNodes[routeNodes.length - 1];
+        if (
+          highestNode &&
+          (routeNodes[0].name === routeNodes[routeNodes.length - 1].name ||
+            routeNodes[0].elevation > lastNode.elevation ||
+            routeNodes[0].elevation > lastNode.elevation - 150)
+        ) {
+          route.weatherSite = {
+            id: highestNode.id,
+            name: highestNode.name,
+          };
+        } else {
+          route.weatherSite = {
+            id: lastNode.id,
+            name: lastNode.name,
+          };
+        }
+
+        console.log(route.weatherSite);
+
+        routes.push(route);
+
+        if (!passedClosedTrail) {
+          break;
+        }
+      }
+    } else {
+      return null;
+    }
+
+    return routes;
   }
 
   getRouteWithSegments(routeNodes: Node[]): {
@@ -404,6 +505,155 @@ export default class PathFinder {
                   node.id === neighbor.id &&
                   node.trail_id === neighbor.trail_id,
               )
+            ) {
+              neighbor.gCost = costToNeighbor;
+              const distanceToEndNode = Math.floor(
+                distance(
+                  [neighborNodeLngLat.lng, neighborNodeLngLat.lat],
+                  [targetNode.lng, targetNode.lat],
+                  {
+                    units: 'meters',
+                  },
+                ),
+              );
+              neighbor.hCost = distanceToEndNode;
+              neighbor.fCost = costToNeighbor + distanceToEndNode;
+              neighbor.parent = currentNode;
+
+              if (
+                !openSet.find(
+                  (node) =>
+                    node.id === neighbor.id &&
+                    node.trail_id === neighbor.trail_id,
+                )
+              ) {
+                openSet.push(neighbor);
+                //   console.log('neighbor', neighbor);
+              }
+            }
+          }
+        });
+      }
+    }
+    return null;
+  }
+
+  findPath2(
+    startNode: Node,
+    targetNode: Node,
+    raw: boolean,
+    avoidClosedTrails: boolean,
+  ): {
+    path: PathSegment | RawPathSegment;
+    passedClosedTrail: boolean;
+  } | null {
+    console.time('Search');
+    let openSet: SegmentNode[] = [];
+    const closedSet: SegmentNode[] = [];
+    let passedClosedTrail = false;
+
+    openSet.push({
+      id: startNode.id,
+      distance: 0,
+      trail_id: 0,
+      gCost: 0,
+      hCost: 0,
+      fCost: 0,
+      parent: null,
+    });
+
+    while (openSet.length > 0) {
+      let currentNode = openSet[0];
+
+      for (let i = 0; i < openSet.length; i++) {
+        const trail = mapFeatures.trails.get(openSet[i].trail_id);
+        const isClosed = (trail && trail.closed) ?? false;
+        if (
+          openSet[i].fCost < currentNode.fCost ||
+          (openSet[i].fCost === currentNode.fCost &&
+            openSet[i].hCost < currentNode.hCost)
+        ) {
+          if (isClosed) {
+            if (avoidClosedTrails) {
+              passedClosedTrail = false;
+              console.log('Skipped closed trail:', openSet[i].trail_id);
+              continue;
+            } else {
+              passedClosedTrail = true;
+            }
+          }
+          console.log(openSet[i].trail_id, passedClosedTrail);
+
+          currentNode = openSet[i];
+        }
+      }
+
+      const trail = mapFeatures.trails.get(currentNode.trail_id);
+      const isClosed = (trail && trail.closed) ?? false;
+      if (currentNode.id === targetNode.id) {
+        if (isClosed && !avoidClosedTrails) {
+          passedClosedTrail = true;
+        }
+
+        console.timeEnd('Search');
+        //   console.log(closedSet);
+        if (!raw) {
+          return { path: this.retracePath(currentNode), passedClosedTrail };
+        }
+        return { path: this.retraceRawPath(currentNode), passedClosedTrail };
+        // return this.retracePath(currentNode);
+      }
+
+      openSet = openSet.filter(
+        (node) =>
+          node.id !== currentNode.id && node.trail_id !== currentNode.trail_id,
+      );
+      closedSet.push(currentNode);
+      // console.log('currentNode', currentNode);
+
+      const neighbors = this.graph.adjacencyList
+        .get(currentNode.id)
+        ?.map<SegmentNode>((node) => ({
+          id: node.node_id,
+          distance: node.distance,
+          trail_id: node.trail_id,
+          gCost: 0,
+          hCost: 0,
+          fCost: 0,
+          parent: null,
+        }));
+
+      const currentNodeLngLat = mapFeatures.nodes.get(currentNode.id);
+      if (currentNodeLngLat) {
+        if (!neighbors) {
+          return null;
+        }
+
+        neighbors.forEach((neighbor) => {
+          if (
+            !closedSet.find(
+              (node) =>
+                node.id === neighbor.id && node.trail_id === neighbor.trail_id,
+            )
+          ) {
+            const neighborNodeLngLat = mapFeatures.nodes.get(neighbor.id);
+            if (!neighborNodeLngLat) {
+              return null;
+            }
+
+            const costToNeighbor = currentNode.gCost + neighbor.distance;
+            //   console.log(costToNeighbor, neighbor.id, neighbor.gCost, neighbor);
+
+            const trail = mapFeatures.trails.get(neighbor.trail_id);
+            const isClosed = (trail && trail.closed) ?? false;
+            if (
+              ((avoidClosedTrails && !isClosed) || !avoidClosedTrails) &&
+              (costToNeighbor < neighbor.gCost ||
+                !openSet.find(
+                  (node) =>
+                    node.id === neighbor.id &&
+                    node.trail_id === neighbor.trail_id,
+                ))
             ) {
               neighbor.gCost = costToNeighbor;
               const distanceToEndNode = Math.floor(
