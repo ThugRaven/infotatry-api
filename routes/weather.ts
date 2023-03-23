b2d7ecf06fa88045dc24e5a06b0d045975bae45d
ThugRaven <kamilo1715@wp.pl>
2023-03-23 15:40:42 +0100

    feat: disable few weather endpoints

--- routes/weather.ts
+++ routes/weather.ts
@@ -6,27 +6,27 @@ dotenv.config();
 const router = express.Router();
 const weather = new Weather(process.env.WEATHER_API_KEY!);
 
-router.get('/coordinates/:cityName', async (req, res) => {
-  const cityName = req.params.cityName;
+// router.get('/coordinates/:cityName', async (req, res) => {
+//   const cityName = req.params.cityName;
 
-  console.log(cityName);
-  const coords = await weather.getCoordinatesByCityName(cityName);
-  console.log(coords);
+//   console.log(cityName);
+//   const coords = await weather.getCoordinatesByCityName(cityName);
+//   console.log(coords);
 
-  res.status(200).send(coords);
-});
+//   res.status(200).send(coords);
+// });
 
-router.get('/current/:lat/:lng', async (req, res) => {
-  const { lat: latitude, lng: longitude } = req.params;
+// router.get('/current/:lat/:lng', async (req, res) => {
+//   const { lat: latitude, lng: longitude } = req.params;
 
-  const currentWeather = await weather.getCurrentWeatherByLatLng(
-    latitude,
-    longitude,
-  );
-  console.log('currentWeather', currentWeather);
+//   const currentWeather = await weather.getCurrentWeatherByLatLng(
+//     latitude,
+//     longitude,
+//   );
+//   console.log('currentWeather', currentWeather);
 
-  res.status(200).send(currentWeather);
-});
+//   res.status(200).send(currentWeather);
+// });
 
 router.get('/current/:weatherSite', async (req, res) => {
   const weatherSite = req.params.weatherSite;
@@ -48,31 +48,31 @@ router.get('/current/:weatherSite', async (req, res) => {
   res.status(200).send(currentWeather);
 });
 
-router.get('/forecast/:lat/:lng', async (req, res) => {
-  const { lat, lng } = req.params;
+// router.get('/forecast/:lat/:lng', async (req, res) => {
+//   const { lat, lng } = req.params;
 
-  const _lat = parseFloat(lat);
-  const _lng = parseFloat(lng);
-  if (isNaN(_lat) || isNaN(_lng)) {
-    res.status(404).send({
-      message: 'Weather site not found',
-    });
-  }
+//   const _lat = parseFloat(lat);
+//   const _lng = parseFloat(lng);
+//   if (isNaN(_lat) || isNaN(_lng)) {
+//     res.status(404).send({
+//       message: 'Weather site not found',
+//     });
+//   }
 
-  const latitude = _lat.toFixed(4);
-  const longitude = _lng.toFixed(4);
+//   const latitude = _lat.toFixed(4);
+//   const longitude = _lng.toFixed(4);
 
-  const weatherForecast = await weather.getWeatherForecastByLatLngWithCache(
-    latitude,
-    longitude,
-  );
-  console.log(
-    'weatherForecast',
-    weatherForecast ? weatherForecast.cod : weatherForecast,
-  );
+//   const weatherForecast = await weather.getWeatherForecastByLatLngWithCache(
+//     latitude,
+//     longitude,
+//   );
+//   console.log(
+//     'weatherForecast',
+//     weatherForecast ? weatherForecast.cod : weatherForecast,
+//   );
 
-  res.status(200).send(weatherForecast);
-});
+//   res.status(200).send(weatherForecast);
+// });
 
 router.get('/forecast/:weatherSite', async (req, res) => {
   const weatherSite = req.params.weatherSite;
